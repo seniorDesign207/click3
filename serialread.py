39f2b10b23171331acdea73346f4100269b1e721
seniorDesign <seniordesign@gmail.com>
2019-03-26 18:38:59 -0500

    Added read file

---
+++ serialread.py
@@ -0,0 +1,16 @@
+import time
+import serial
+
+ser = serial.Serial(
+	port='/dev/serial0',
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+)
+counter=0
+
+while 1:
+	if(ser.in_waiting > 0):
+		x=ser.read(1)
+        	print x
