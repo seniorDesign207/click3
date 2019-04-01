7f8a193413e313f578262c930ed787f88ddd3ef1
seniorDesign207 <seniordesign207@gmail.com>
2019-04-01 04:26:54 -0500

    x

--- serialwrite.py
+++ serialwrite.py
@@ -14,10 +14,10 @@ counter=0
 
 
 while 1:
-    	ser.write(b'%d' % (counter))
-        time.sleep(0.05)
-        counter += 1
+    ser.write(b'%d' % (counter))
+    time.sleep(0.05)
+    counter += 1
 
-        if (ser.in_waiting > 0):
-            x = ser.read(1)
-            print("Just Received : %d" % x)
+    if (ser.in_waiting > 0):
+        x = ser.read(1)
+        print("Just Received : %d" % x)
